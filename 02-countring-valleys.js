af5b148f5894a0452b76b4f3fedb7a376e347df5
Anthony Bird <anthony@responsive.co.za>
2020-08-22 01:16:51 +0200

    counting valleys

--- 02-countring-valleys.js
+++ 02-countring-valleys.js
@@ -8,20 +8,28 @@ function countingValleys(n, s) {
   let step = 0;
   const steps = [0];
   const valleys = [0];
+  const negatives = [];
   let count = 0;
   for (let position of s) {
     count++;
     position === "U" ? step++ : step--;
     steps.push(step);
     // Get the previous step in the array, if it's negative we know that it is a valley..
-    if (Math.sign(steps[steps.length - 2]) === -1 && step === 0) {
-      valleys.push(step);
+    if (Math.sign(steps[steps.length - 2]) === -1) {
+      negatives.push(step);
+      if (step === 0) {
+        valleys.push(step);
+      }
     }
     // Add the last step
     if (count === s.length && Math.sign(steps[steps.length - 2]) === 1) {
       valleys.push(step);
       console.log("last char", position);
     }
+  }
+
+  if (valleys.length === 2 && negatives.length === 0) {
+    valleys = [];
   }
   return Math.floor(valleys.length / 2);
 }
